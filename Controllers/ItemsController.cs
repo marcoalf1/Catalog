d1686401e98cad8f24cd623b7fbf4bf8f6ba508c
Marco Hernandez <ma.hl@outlook.com>
2022-04-29 23:01:57 -0600

    DTOs, HTTP methods

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -26,6 +26,7 @@ namespace Catalog.Controllers
             return items;
         }
 
+        // GET /items/{id}
         [HttpGet("{id}")]
         public ActionResult<ItemDTO> GetItem(Guid id)
         {
@@ -35,5 +36,59 @@ namespace Catalog.Controllers
 
             return item.AsDTO();
         }
+
+        // POST /items
+        [HttpPost]
+        public ActionResult<ItemDTO> CreateItem(CreateItemDTO itemDTO)
+        {
+            Item item = new (){
+                Id = Guid.NewGuid(),
+                Name = itemDTO.Name,
+                Price = itemDTO.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id}, item.AsDTO());
+
+        } 
+
+        // PUT /items/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDTO itemDTO)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null) 
+            {
+                return NotFound();
+            }
+             
+            Item updatedItem = existingItem with 
+            {
+                Name = itemDTO.Name,
+                Price = itemDTO.Price
+            };
+
+            repository.UpdateItem(updatedItem);
+
+            return NoContent();
+
+        }
+
+         // DELETE /items/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null) 
+            {
+                return NotFound();
+            }
+
+            repository.DeleteItem(id);
+            return NoContent();
+        }
+
     }
 }
