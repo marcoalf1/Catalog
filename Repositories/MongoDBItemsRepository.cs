87b80403c56054adb2f74a075b5f06885f76e9df
Marco Hernandez <ma.hl@outlook.com>
2022-04-29 23:34:44 -0600

    Actualizacion

---
+++ Repositories/MongoDBItemsRepository.cs
@@ -0,0 +1,32 @@
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class MongoDBItemsRepository : IItemsRepository
+    {
+        public void CreateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
