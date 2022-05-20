13251e1fc747fbe674ea10a823d249bce63566e9
Marco Hernandez <ma.hl@outlook.com>
2022-05-19 19:44:48 -0600

    preparando para docker

--- Program.cs
+++ Program.cs
@@ -50,9 +50,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHttpsRedirection(); 
 }
 
-app.UseHttpsRedirection();
+
 
 app.UseAuthorization();
 
